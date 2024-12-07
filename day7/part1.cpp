66f1a1fe1deb2ce96a56a23356ce687e36761dd6
ConradWarren <conrad_warren11@outlook.com>
2024-12-07 16:12:48 -0700

    Added day 7

---
+++ day7/part1.cpp
@@ -0,0 +1,29 @@
+#include "../core.h"
+
+bool Can_Solve(std::vector<long long>& nums, int idx, long long last_valid, long long target){
+    if(idx >= nums.size()){
+        return (target == last_valid);
+    }
+    if(last_valid > target) return false;
+    bool add = Can_Solve(nums, idx+1, last_valid + nums[idx], target);
+    bool multiply = Can_Solve(nums, idx+1, last_valid * nums[idx], target);
+    
+    return add || multiply;
+}
+
+int main() {
+
+    std::vector<std::vector<long long>> input_data;
+    Read_Data("../input.txt", input_data);
+    long long result = 0;
+    
+    for(int i = 0; i < input_data.size(); i++){
+        if(Can_Solve(input_data[i], 2, input_data[i][1], input_data[i][0])){
+            result += input_data[i][0];
+        }
+    }
+    
+    std::cout<<result<<'\n';
+    
+    return 0;
+}
