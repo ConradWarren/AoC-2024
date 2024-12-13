8a90c40f12ba8f9cdff5b5e31b8aa471e127cdf1
ConradWarren <conrad_warren11@outlook.com>
2024-12-13 11:22:45 -0700

    Got the days mixed up, fixed them

--- day11/part2.cpp
+++ day11/part2.cpp
@@ -1,79 +1,37 @@
-#include "../core.h"
+#include "core.h"
 
-inline bool Is_Edge(std::vector<std::string>& grid, int y, int x, char target){
-    return (y < 0 || x < 0 || y >= grid.size() || x >= grid[0].size() || grid[y][x] != target);
-}
+long long Eval(long long num, int depth, std::vector<std::unordered_map<long long, long long>>& cache){
 
-long long Breadth_First_Search(std::vector<std::string>& grid, int y, int x, std::vector<std::vector<bool>>& seen){
-
-    std::vector<std::vector<bool>> counted(grid.size(), std::vector<bool>(grid[0].size(), false));
-    char target = grid[y][x];
-
-    long long area = 0;
-    long long sides = 0;
-
-    seen[y][x] = true;
-
-    std::queue<std::pair<int, int>> node_queue;
-    node_queue.push({y,x});
-    std::vector<std::pair<int, int>> directions = {{0,1},{1,0},{0,-1},{-1, 0}};
-    while (!node_queue.empty()){
-
-        y = node_queue.front().first;
-        x = node_queue.front().second;
-        area++;
-
-        bool up = Is_Edge(grid, y-1, x, target);
-        bool down = Is_Edge(grid, y+1, x, target);
-        bool right = Is_Edge(grid, y, x+1, target);
-        bool left = Is_Edge(grid, y, x-1, target);
-
-        if(up && right) sides++;
-        if(up && left) sides++;
-        if(down && right) sides++;
-        if(down && left) sides++;
-        if(!up && !right && Is_Edge(grid, y-1, x+1, target)) sides++;
-        if(!up && !left && Is_Edge(grid, y-1, x-1, target)) sides++;
-        if(!down && !right && Is_Edge(grid, y+1, x+1, target)) sides++;
-        if(!down && !left && Is_Edge(grid, y+1, x-1, target)) sides++;
-
-        node_queue.pop();
-
-        for(int i = 0; i < directions.size(); i++){
-
-            int ty = y + directions[i].first;
-            int tx = x + directions[i].second;
-
-            if(ty < 0 || tx < 0 || ty >= grid.size() || tx >= grid[0].size()){
-                continue;
-            }
-
-            if(grid[ty][tx] == target && !seen[ty][tx]){
-                seen[ty][tx] = true;
-                node_queue.push({ty,tx});
-            }
-        }
+    if(depth == 75){
+        return 1;
     }
-    
-    return area * sides;
+
+    if(cache[depth].find(num) != cache[depth].end()){
+        return cache[depth][num];
+    }
+
+    if(num == 0){
+        return cache[depth][num] = Eval(1, depth+1, cache);
+    }
+
+    int digits = static_cast<int>(std::log10(num))+1;
+    if(digits%2 == 0){
+        return cache[depth][num] = Eval(num%(static_cast<int>(std::pow(10, digits/2))), depth+1, cache) + Eval(num/(static_cast<int>(std::pow(10, digits/2))), depth+1, cache);
+    }
+
+
+    return cache[depth][num] = Eval(num * 2024, depth+1, cache);
 }
 
 int main() {
 
-    std::vector<std::string> input_data;
-    Read_Data("../input.txt", input_data);
-
-    std::vector<std::vector<bool>> seen(input_data.size(), std::vector<bool>(input_data[0].size(), false));
-
+    std::vector<long long> nums;
+    Read_Data("../input.txt", nums);
+    std::vector<std::unordered_map<long long, long long>> cache(75);
     long long result = 0;
-    for(int i = 0; i < input_data.size(); i++){
-        for(int j = 0; j < input_data[0].size(); j++){
 
-            if(!seen[i][j]){
-                result += Breadth_First_Search(input_data, i, j, seen);
-            }
-
-        }
+    for(int i = 0; i < nums.size(); i++){
+        result += Eval(nums[i], 0, cache);
     }
 
     std::cout<<result<<'\n';
--- day12/part2.cpp
+++ day12/part2.cpp
@@ -1,48 +1,81 @@
 #include "../core.h"
 
-long long Get_Min(long long Tx, long long Ty, long long Ax, long long Ay, long long Bx, long long By){
+inline bool Is_Edge(std::vector<std::string>& grid, int y, int x, char target){
+    return (y < 0 || x < 0 || y >= grid.size() || x >= grid[0].size() || grid[y][x] != target);
+}
 
-    long long det = Ay * Bx - Ax * By;
-    if(det == 0){
-        if((Ty%By != 0 || Tx%Bx != 0) && (Ty%Ay != 0 || Tx%Ax != 0)) return 0;
-        if((By * 3 >= Ay)) return Ty/By;
-        return (Ty/Ay)*3 + (Ty%Ay)/By;
+long long Breadth_First_Search(std::vector<std::string>& grid, int y, int x, std::vector<std::vector<bool>>& seen){
+
+    std::vector<std::vector<bool>> counted(grid.size(), std::vector<bool>(grid[0].size(), false));
+    char target = grid[y][x];
+
+    long long area = 0;
+    long long sides = 0;
+
+    seen[y][x] = true;
+
+    std::queue<std::pair<int, int>> node_queue;
+    node_queue.push({y,x});
+    std::vector<std::pair<int, int>> directions = {{0,1},{1,0},{0,-1},{-1, 0}};
+    while (!node_queue.empty()){
+
+        y = node_queue.front().first;
+        x = node_queue.front().second;
+        area++;
+
+        bool up = Is_Edge(grid, y-1, x, target);
+        bool down = Is_Edge(grid, y+1, x, target);
+        bool right = Is_Edge(grid, y, x+1, target);
+        bool left = Is_Edge(grid, y, x-1, target);
+
+        if(up && right) sides++;
+        if(up && left) sides++;
+        if(down && right) sides++;
+        if(down && left) sides++;
+        if(!up && !right && Is_Edge(grid, y-1, x+1, target)) sides++;
+        if(!up && !left && Is_Edge(grid, y-1, x-1, target)) sides++;
+        if(!down && !right && Is_Edge(grid, y+1, x+1, target)) sides++;
+        if(!down && !left && Is_Edge(grid, y+1, x-1, target)) sides++;
+
+        node_queue.pop();
+
+        for(int i = 0; i < directions.size(); i++){
+
+            int ty = y + directions[i].first;
+            int tx = x + directions[i].second;
+
+            if(ty < 0 || tx < 0 || ty >= grid.size() || tx >= grid[0].size()){
+                continue;
+            }
+
+            if(grid[ty][tx] == target && !seen[ty][tx]){
+                seen[ty][tx] = true;
+                node_queue.push({ty,tx});
+            }
+        }
     }
-
-
-    long long det1 = Ty * Bx - Tx * By;
-    long long det2 = Ay * Tx - Ax * Ty;
-
-    if(det1%det != 0 || det2%det != 0){
-        return 0;
-    }
-
-    long long k1 = det1/det;
-    long long k2 = det2/det;
-
-    return k1*3 + k2;
+    
+    return area * sides;
 }
 
 int main() {
 
-    std::vector<std::vector<long long>> input_data;
+    std::vector<std::string> input_data;
     Read_Data("../input.txt", input_data);
 
+    std::vector<std::vector<bool>> seen(input_data.size(), std::vector<bool>(input_data[0].size(), false));
+
     long long result = 0;
-    for(int i = 0; i < input_data.size(); i += 4){
+    for(int i = 0; i < input_data.size(); i++){
+        for(int j = 0; j < input_data[0].size(); j++){
 
-        if(input_data.empty()) continue;
+            if(!seen[i][j]){
+                result += Breadth_First_Search(input_data, i, j, seen);
+            }
 
-        long long Ty = input_data[i+2][1] + 10000000000000;
-        long long Tx = input_data[i+2][0] + 10000000000000;
-        long long Ax = input_data[i][0];
-        long long Ay = input_data[i][1];
-        long long Bx = input_data[i+1][0];
-        long long By = input_data[i+1][1];
-
-        result += Get_Min(Tx, Ty, Ax, Ay, Bx, By);
-
+        }
     }
+
     std::cout<<result<<'\n';
     return 0;
 }
